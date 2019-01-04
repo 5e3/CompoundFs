f9c785a8596d2608cbbf55abc420e2bfb226893c
5e3 <49342543+5e3@users.noreply.github.com>
2019-01-04 07:57:39 +0100

    PageAllocator with test cases plus some skeleton for the CacheManager

---
+++ TestDriver/TestPageAllocator.cpp
@@ -0,0 +1,108 @@
+
+
+#include "stdafx.h"
+#include <set>
+#include <memory>
+#include <algorithm>
+#include <unordered_set>
+#include "Test.h"
+#include "PageAllocator.h"
+
+using namespace CompFs;
+
+TEST(PageAllocator, allocReturnsWritableMemory)
+{
+    PageAllocator alloc(16);
+    auto ptr = alloc.allocate();
+    *ptr = 1;
+}
+
+TEST(PageAllocator, CanAllocateManyPages)
+{
+    PageAllocator alloc(16);
+    std::vector<std::shared_ptr<uint8_t>> pages;
+    for (int i=0; i<64; i++)
+        pages.push_back(alloc.allocate());
+
+    std::unordered_set<std::shared_ptr<uint8_t>> pset;
+    for (auto p: pages)
+        CHECK(pset.insert(p).second); // check if unique
+}
+
+TEST(PageAllocator, allocReusesMemory)
+{
+    PageAllocator alloc(16);
+
+    std::unordered_set<uint8_t*> pset;
+    {
+        std::vector<std::shared_ptr<uint8_t>> pages;
+        for (int i=0; i<70; i++)
+        {
+            pages.push_back(alloc.allocate());
+            pset.insert(pages.back().get());
+        }
+    }
+
+    std::vector<std::shared_ptr<uint8_t>> pages;
+    for (int i=0; i<70; i++)
+    {
+        pages.push_back(alloc.allocate());
+        CHECK(pset.count(pages.back().get()) != 0);
+    }
+}
+
+TEST(PageAllocator, trimFreesAllButOnePageWithoutOutstandingReferences)
+{
+    PageAllocator alloc(16);
+
+    {
+        std::vector<std::shared_ptr<uint8_t>> pages;
+        for (int i=0; i<200; i++)
+            pages.push_back(alloc.allocate());
+        std::random_shuffle(pages.begin(), pages.end());
+    }
+
+    auto statistic = alloc.trim();
+    CHECK(statistic.first == 1);
+    CHECK(statistic.second < 16);
+}
+
+TEST(PageAllocator, trimFreesWhatIsUsefull)
+{
+    PageAllocator alloc(16);
+    auto p = alloc.allocate();  // hold the first page
+
+    {
+        std::vector<std::shared_ptr<uint8_t>> pages;
+        for (int i=0; i<200; i++)
+            pages.push_back(alloc.allocate());
+        std::random_shuffle(pages.begin(), pages.end());
+    }
+
+    auto statistic = alloc.trim();
+    CHECK(statistic.first == 2);
+    CHECK(statistic.second > 16);
+    CHECK(statistic.second < 32);
+
+    p.reset();
+    statistic = alloc.trim();
+    CHECK(statistic.first == 1);
+    CHECK(statistic.second < 16);
+}
+
+TEST(PageAllocator, stressTest)
+{
+    PageAllocator alloc(512);
+
+    {
+        std::vector<std::shared_ptr<uint8_t>> pages;
+        for (int i=0; i<5120; i++)
+            pages.push_back(alloc.allocate());
+        std::random_shuffle(pages.begin(), pages.end());
+    }
+
+    auto statistic = alloc.trim();
+    CHECK(statistic.first == 1);
+    CHECK(statistic.second < 16);
+
+}
