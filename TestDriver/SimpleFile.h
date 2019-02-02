e7e9e0211c2eff15d21e400554624486a3154fa3
Nils Antonsen <nils.antonsen@active.ch>
2019-02-02 17:41:48 +0100

    forgot a file

---
+++ TestDriver/SimpleFile.h
@@ -0,0 +1,39 @@
+
+#pragma once
+
+#include "../CompoundFs/CacheManager.h"
+#include "../CompoundFs/PageAllocator.h"
+#include <memory>
+
+namespace TxFs
+{
+struct SimpleFile : RawFileInterface
+{
+    SimpleFile()
+        : m_allocator(512)
+    {}
+
+    virtual PageIndex newPage() override
+    {
+        PageIndex id = (PageIndex) m_file.size();
+        m_file.push_back(m_allocator.allocate());
+        return id;
+    }
+
+    virtual void writePage(PageIndex id, std::shared_ptr<uint8_t> page) override
+    {
+        auto p = m_file.at(id);
+        std::copy(page.get(), page.get() + 4096, p.get());
+    }
+
+    virtual void readPage(PageIndex id, std::shared_ptr<uint8_t> page) const override
+    {
+        auto p = m_file.at(id);
+        std::copy(p.get(), p.get() + 4096, page.get());
+    }
+
+    std::vector<std::shared_ptr<uint8_t>> m_file;
+    PageAllocator m_allocator;
+};
+
+}
