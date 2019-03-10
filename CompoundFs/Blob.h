0fc9d8b6adcf9b70ed68bea264a5ca001847a3c2
5e3 <49342543+5e3@users.noreply.github.com>
2019-03-10 16:36:54 +0100

    cosmetics

--- CompoundFs/Blob.h
+++ CompoundFs/Blob.h
@@ -12,107 +12,105 @@
 namespace TxFs
 {
 
-    class BlobRef
+class BlobRef
+{
+    inline static uint8_t g_default = 0;
+
+public:
+    BlobRef()
+        : m_data(&g_default)
+    {}
+
+    BlobRef(const uint8_t* val)
+        : m_data((uint8_t*) val)
+    {}
+
+    uint16_t size() const { return *m_data + 1; }
+    const uint8_t* begin() const { return m_data; }
+    const uint8_t* end() const { return begin() + size(); }
+    bool operator==(const BlobRef& lhs) const { return std::equal(begin(), end(), lhs.begin()); }
+    bool operator!=(const BlobRef& lhs) const { return !(*this == lhs); }
+    bool operator<(const BlobRef& rhs) const
     {
-        inline static uint8_t g_default = 0;
-    public:
-        BlobRef() : m_data(&g_default)
-        {
-        }
+        return std::lexicographical_compare(begin() + 1, end(), rhs.begin() + 1, rhs.end());
+    }
 
-        BlobRef(const uint8_t* val)
-            : m_data((uint8_t*)val)
-        {}
+    uint8_t* begin() { return m_data; }
+    uint8_t* end() { return begin() + size(); }
 
-        uint16_t size() const { return *m_data + 1; }
-        const uint8_t* begin() const { return m_data; }
-        const uint8_t* end() const { return begin() + size(); }
-        bool operator==(const BlobRef& lhs) const { return std::equal(begin(), end(), lhs.begin()); }
-        bool operator!=(const BlobRef& lhs) const { return !(*this == lhs); }
-        bool operator<(const BlobRef& rhs) const
-        {
-            return std::lexicographical_compare(begin() + 1, end(), rhs.begin() + 1, rhs.end());
-        }
+protected:
+    uint8_t* m_data;
+};
 
-        uint8_t* begin() { return m_data; }
-        uint8_t* end() { return begin() + size(); }
+//////////////////////////////////////////////////////////////////////////
 
+class Blob : public BlobRef
+{
+public:
+    Blob() = default;
 
-    protected:
-        uint8_t* m_data;
-    };
-
-    //////////////////////////////////////////////////////////////////////////
-
-
-    class Blob : public BlobRef
+    Blob(const char* str)
+        : m_container(strlen(str) + 1)
     {
-    public:
-        Blob() = default;
+        m_data = &m_container[0];
+        m_data[0] = uint8_t(m_container.size() - 1);
+        for (size_t i = 1; i < m_container.size(); i++)
+            m_container[i] = (uint8_t) str[i - 1];
+    }
 
-        Blob(const char* str)
-            : m_container(strlen(str) + 1)
-        {
-            m_data = &m_container[0];
-            m_data[0] = uint8_t(m_container.size() - 1);
-            for (size_t i = 1; i < m_container.size(); i++)
-                m_container[i] = (uint8_t)str[i - 1];
-        }
+    Blob(const uint8_t* val)
+        : m_container(*val + size_t(1))
+    {
+        m_data = &m_container[0];
+        std::copy(val, val + m_container.size(), m_data);
+    }
 
-        Blob(const uint8_t* val)
-            : m_container(*val + 1)
-        {
-            m_data = &m_container[0];
-            std::copy(val, val + m_container.size(), m_data);
-        }
+    Blob(const BlobRef& br)
+        : Blob(br.begin())
+    {}
 
-        Blob(const BlobRef& br)
-            : Blob(br.begin())
-        {
-        }
+    Blob(const Blob& val)
+        : m_container(val.m_container)
+    {
+        m_data = &m_container[0];
+    }
 
-        Blob(const Blob& val)
-            : m_container(val.m_container)
-        {
-            m_data = &m_container[0];
-        }
+    Blob(Blob&& val) noexcept
+        : m_container(std::move(val.m_container))
+    {
+        m_data = &m_container[0];
+    }
 
-        Blob(Blob&& val)
-            : m_container(std::move(val.m_container))
-        {
-            m_data = &m_container[0];
-        }
+    Blob& operator=(const Blob& val)
+    {
+        auto tmp = val;
+        swap(tmp);
+        return *this;
+    }
 
-        Blob& operator=(const Blob& val)
-        {
-            auto tmp = val;
-            swap(tmp);
-            return *this;
-        }
+    Blob& operator=(const BlobRef& ref)
+    {
+        Blob val(ref);
+        swap(val);
+        return *this;
+    }
 
-        Blob& operator=(const BlobRef& ref)
-        {
-            Blob val(ref);
-            swap(val);
-            return *this;
-        }
+    Blob& operator=(Blob&& val) noexcept
+    {
+        m_container = std::move(val.m_container);
+        m_data = &m_container[0];
+        return *this;
+    }
 
-        Blob& operator=(Blob&& val)
-        {
-            m_container = std::move(val.m_container);
-            m_data = &m_container[0];
-            return *this;
-        }
+    void swap(Blob& val)
+    {
+        std::swap(m_container, val.m_container);
+        std::swap(m_data, val.m_data);
+    }
 
-        void swap(Blob& val)
-        {
-            std::swap(m_container, val.m_container);
-            std::swap(m_data, val.m_data);
-        }
-
-    private:
-        std::vector<uint8_t> m_container;
-    };
+private:
+    std::vector<uint8_t> m_container;
+};
 
 //////////////////////////////////////////////////////////////////////////
 
