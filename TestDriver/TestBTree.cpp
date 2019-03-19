2dd912d6f0823463782467f113523bfe558ffbc2
5e3 <49342543+5e3@users.noreply.github.com>
2019-03-19 21:14:13 +0100

    some cool changes to BTree and relatives. BTree is now more consistant but beware remove() is complete useless.

--- TestDriver/TestBTree.cpp
+++ TestDriver/TestBTree.cpp
@@ -63,27 +63,78 @@ TEST(BTree, insertReplacesOriginal)
     Blob value("Te$tData");
     bt.insert("2233", value);
     auto res = bt.find("2233");
-    CHECK(value == res);
+    CHECK(value == res.value());
 
     // value has different size => remove, add
     value = Blob("Data");
     bt.insert("1122", value);
     res = bt.find("1122");
-    CHECK(value == res);
+    CHECK(value == res.value());
 }
 
-TEST(BTree, EmptyTreeReturnsDoneCursor)
+TEST(BTree, insertNewKeyInsertsAndReturnsInserted)
+{
+    SimpleFile sf;
+    auto cm = std::make_shared<CacheManager>(&sf);
+    BTree bt(cm);
+
+    for (size_t i = 0; i < 3000; i++)
+    {
+        std::string s = std::to_string(i);
+        bt.insert(s.c_str(), "TestData");
+    }
+
+    auto res = bt.insert("TestKey", "TestValue", [](const BlobRef&, const BlobRef&) {
+        throw std::runtime_error("");
+        return true;
+    });
+
+    CHECK(std::holds_alternative<BTree::Inserted>(res));
+}
+
+TEST(BTree, canControlReplacementWithStrategy)
+{
+    SimpleFile sf;
+    auto cm = std::make_shared<CacheManager>(&sf);
+    BTree bt(cm);
+
+    for (size_t i = 0; i < 1000; i++)
+    {
+        std::string s = std::to_string(i);
+        bt.insert(s.c_str(), "TestData");
+    }
+
+    bt.insert("TestKey", "TestValue", [](const BlobRef&, const BlobRef&) {
+        throw std::runtime_error("");
+        return true;
+    });
+
+    auto res = bt.insert("TestKey", "TestValue1", [](const BlobRef&, const BlobRef&) {
+        return false;
+    });
+
+    CHECK(std::get<BTree::Unchanged>(res).m_currentValue.current().second == Blob("TestValue"));
+
+    res = bt.insert("TestKey", "TestValue2", [](const BlobRef&, const BlobRef&) {
+        return true;
+    });
+
+    CHECK(std::get<BTree::Replaced>(res).m_beforeValue == Blob("TestValue"));
+    CHECK(bt.find("TestKey").value() == Blob("TestValue2"));
+}
+
+TEST(BTree, emptyTreeReturnsFalseCursor)
 {
     SimpleFile sf;
     auto cm = std::make_shared<CacheManager>(&sf);
     BTree bt(cm);
 
     auto cur = bt.begin("");
-    CHECK(cur.done());
-    CHECK(bt.next(cur).done());
+    CHECK(!cur);
+    CHECK(!bt.next(cur));
 }
 
-TEST(BTree, CursorPointsToCurrentItem)
+TEST(BTree, cursorPointsToCurrentItem)
 {
     SimpleFile sf;
     auto cm = std::make_shared<CacheManager>(&sf);
@@ -98,11 +149,9 @@ TEST(BTree, CursorPointsToCurrentItem)
     auto cur = bt.begin("100");
     CHECK(cur.current().first == Blob("100"));
     CHECK(cur.current().second == Blob("100 Test"));
-
-    
 }
 
-TEST(BTree, CursorIterates)
+TEST(BTree, cursorIterates)
 {
     SimpleFile sf;
     auto cm = std::make_shared<CacheManager>(&sf);
@@ -117,14 +166,14 @@ TEST(BTree, CursorIterates)
     auto cur = bt.begin("");
     for (size_t i = 0; i < 500; i++)
     {
-        CHECK(!cur.done());
+        CHECK(cur);
         cur = bt.next(cur);
     }
 
-    CHECK(cur.done());
+    CHECK(!cur);
 }
 
-TEST(BTree, CursorNextPointsToNext)
+TEST(BTree, cursorNextPointsToNext)
 {
     SimpleFile sf;
     auto cm = std::make_shared<CacheManager>(&sf);
@@ -141,7 +190,7 @@ TEST(BTree, CursorNextPointsToNext)
     CHECK(cur.current().first == Blob("101"));
 }
 
-TEST(BTree, CursorKeepsPageInMemory)
+TEST(BTree, cursorKeepsPageInMemory)
 {
     SimpleFile sf;
     auto cm = std::make_shared<CacheManager>(&sf);
@@ -163,5 +212,55 @@ TEST(BTree, CursorKeepsPageInMemory)
     cur = Cursor();
     pagesStillInMem = cm->trim(0);
     CHECK(pagesStillInMem == 0);
+}
+
+TEST(BTree, removeAllKeysLeavesTreeEmpty)
+{
+    SimpleFile sf;
+    auto cm = std::make_shared<CacheManager>(&sf);
+    BTree bt(cm);
+
+    std::vector<uint32_t> keys;
+    keys.reserve(3000);
+    for (uint32_t i = 0; i < 3000; i++)
+        keys.push_back(i);
+
+    for (auto key : keys)
+    {
+        std::string s = std::to_string(key);
+        bt.insert(s.c_str(), "TestData");
+    }
+
+    for (auto key : keys)
+    {
+        std::string s = std::to_string(key);
+        auto res = bt.remove(s.c_str());
+        CHECK(res);
+        CHECK(res == Blob("TestData"));
+    }
+
+    CHECK(!bt.begin(""));
+
+}
+
+TEST(BTree, removeNonExistantKeyReturnsEmptyOptional)
+{
+    SimpleFile sf;
+    auto cm = std::make_shared<CacheManager>(&sf);
+    BTree bt(cm);
+
+    std::vector<uint32_t> keys;
+    keys.reserve(500);
+    for (uint32_t i = 0; i < 500; i++)
+        keys.push_back(i);
+
+    for (auto key : keys)
+    {
+        std::string s = std::to_string(key);
+        bt.insert(s.c_str(), (s + " Test").c_str());
+    }
+
+    CHECK(!bt.remove("Test"));
+    CHECK(bt.remove("399").value() == Blob("399 Test"));
 }
 
