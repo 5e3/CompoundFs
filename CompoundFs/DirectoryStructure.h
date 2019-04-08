ff06f3cc8421ea9610498d0616e576de684c3939
5e3 <49342543+5e3@users.noreply.github.com>
2019-04-08 17:30:11 +0200

    needs some rethinking

--- CompoundFs/DirectoryStructure.h
+++ CompoundFs/DirectoryStructure.h
@@ -25,6 +25,9 @@ public:
     std::optional<Folder> subFolder(std::string_view name, Folder folder = Root) const;
     size_t removeFolder(Folder folder);
 
+    std::optional<FileDescriptor> createFile(std::string_view name, Folder folder = Root);
+    std::optional<FileDescriptor> appendFile(std::string_view name, Folder folder = Root);
+
 private:
     std::shared_ptr<CacheManager> m_cacheManager;
     BTree m_btree;
