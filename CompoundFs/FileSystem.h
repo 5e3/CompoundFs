566a4df611a5fa7de486e79f1880b5ea31eb06b9
5e3 <49342543+5e3@users.noreply.github.com>
2019-04-18 20:31:19 +0200

    Some intermediate ideas

--- CompoundFs/FileSystem.h
+++ CompoundFs/FileSystem.h
@@ -4,16 +4,63 @@
 
 namespace TxFs
 {
+enum class WriteFile : uint32_t;
+enum class ReadFile : uint32_t;
+
+//////////////////////////////////////////////////////////////////////////
+
+struct Path
+{
+    static constexpr Folder AbsoluteRoot = DirectoryKey::Root;
+
+    Path(std::string_view absolutePath)
+        : m_relativePath(absolutePath)
+        , m_root(AbsoluteRoot)
+    {}
+
+    Path(Folder root, std::string_view relativePath)
+        : m_relativePath(relativePath)
+        , m_root(root)
+    {}
+
+    std::string_view m_relativePath;
+    Folder m_root;
+};
+
+//////////////////////////////////////////////////////////////////////////
+
+class PathWalker
+{
+public:
+    std::optional<ByteStringView> expandPath(Path path, const DirectoryStructure* ds);
+    std::optional<ByteStringView> createPath(Path path, DirectoryStructure* ds);
+
+private:
+    DirectoryKey m_dkey;
+};
+
+//////////////////////////////////////////////////////////////////////////
 
 class FileSystem
 {
 public:
-    int createFile(const DirectoryKey& dkey);
-    int appendFile(const DirectoryKey& dkey);
-    int readFile(const DirectoryKey& dkey);
+    WriteFile createFile(Path path);
+    WriteFile appendFile(Path path);
+    ReadFile readFile(Path path) const;
+
+    size_t read(ReadFile file, void* ptr, size_t size) const;
+    size_t write(WriteFile file, const void* ptr, size_t size);
+
+    void close(WriteFile file);
+    void close(ReadFile file);
 
 private:
-    //std::vector<
+    std::optional<Path> expandPath(Path path) const;
+    std::optional<Path> createPath(Path path);
+
+private:
+    DirectoryStructure m_directoryStructure;
+    // std::vector<
 };
 
 }
