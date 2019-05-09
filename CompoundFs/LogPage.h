9ac15a8f7e471c4f21eb6de0c32a4693c62a741e
5e3 <49342543+5e3@users.noreply.github.com>
2019-05-09 21:07:13 +0200

    even some more LogPage

--- CompoundFs/LogPage.h
+++ CompoundFs/LogPage.h
@@ -6,58 +6,77 @@
 
 namespace TxFs
 {
-    using PageIndex = uint32_t;
+using PageIndex = uint32_t;
 
-    class LogPage
+class LogPage
+{
+public:
+    struct PageCopies
     {
-    public:
-        struct PageCopies
-        {
-            uint32_t m_original;
-            uint32_t m_copy;
-        };
-
-    private:
-        uint32_t m_signature[3];
-        uint32_t m_size;
-        PageCopies m_pageCopies[510];
-
-    public:
-        LogPage(PageIndex pageIndex)
-            : m_size(0)
-        {
-            std::minstd_rand mt(pageIndex);
-            m_signature[0] = mt();
-            m_signature[1] = mt();
-            m_signature[2] = mt();
-        }
-
-        bool checkSignature(PageIndex pageIndex) const
-        {
-            std::minstd_rand mt(pageIndex);
-            uint32_t sig[3] = { mt(), mt(), mt()};
-            return std::equal(sig, sig + 3, m_signature, m_signature + 3);
-        }
-
-        std::vector<PageCopies> getPageCopies() const
-        {
-            return std::vector<PageCopies>(&m_pageCopies[0], &m_pageCopies[m_size]);
-        }
-
-        bool pushBack(PageCopies pc)
-        {
-            m_pageCopies[m_size++] = pc;
-            return m_size <= 510;
-        }
-
-
+        uint32_t m_original;
+        uint32_t m_copy;
     };
 
-    constexpr bool operator ==(LogPage::PageCopies lhs, LogPage::PageCopies rhs) noexcept
+    constexpr static size_t MAX_ENTRIES = 510;
+
+private:
+    uint32_t m_signature[3];
+    uint32_t m_size;
+    PageCopies m_pageCopies[MAX_ENTRIES];
+
+public:
+    LogPage(PageIndex pageIndex) noexcept
+        : m_size(0)
     {
-        return lhs.m_copy == rhs.m_copy && lhs.m_original == rhs.m_original;
+        std::minstd_rand mt(pageIndex);
+        m_signature[0] = mt();
+        m_signature[1] = mt();
+        m_signature[2] = mt();
     }
 
+    bool checkSignature(PageIndex pageIndex) const noexcept
+    {
+        std::minstd_rand mt(pageIndex);
+        uint32_t sig[3] = { mt(), mt(), mt() };
+        return std::equal(sig, sig + 3, m_signature, m_signature + 3);
+    }
 
-    static_assert(sizeof(LogPage) == 4096);
+    std::vector<PageCopies> getPageCopies() const
+    {
+        return std::vector<PageCopies>(&m_pageCopies[0], &m_pageCopies[m_size]);
+    }
+
+    constexpr bool pushBack(PageCopies pc) noexcept
+    {
+        if (m_size < MAX_ENTRIES)
+        {
+            m_pageCopies[m_size++] = pc;
+            return true;
+        }
+        return false;
+    }
+
+    template <typename ITERATOR>
+    constexpr bool pushBack(ITERATOR begin, ITERATOR end) noexcept
+    {
+        size_t size = std::distance(begin, end);
+        if (m_size + size <= MAX_ENTRIES)
+        {
+            for (size_t i = m_size; begin != end; ++begin, i++)
+                m_pageCopies[i] = *begin;
+            m_size += static_cast<uint32_t>(size);
+            return true;
+        }
+        return false;
+    }
+
+    constexpr size_t size() const noexcept { return m_size; }
+};
+
+constexpr bool operator==(LogPage::PageCopies lhs, LogPage::PageCopies rhs) noexcept
+{
+    return lhs.m_copy == rhs.m_copy && lhs.m_original == rhs.m_original;
+}
+
+static_assert(sizeof(LogPage) == 4096);
 }
--- TestDriver/TestLogPage.cpp
+++ TestDriver/TestLogPage.cpp
@@ -18,17 +18,40 @@ TEST(LogPage, checkSignature)
     CHECK(!log.checkSignature(1));
 }
 
-
 TEST(LogPage, storingRetrieving)
 {
     LogPage lp(100);
 
     auto pageCopies = lp.getPageCopies();
     for (uint32_t i = 0; i < 500; i++)
-        pageCopies.push_back({ i,i });
+        pageCopies.push_back({ i, i });
 
-    for (auto pc : pageCopies)
+    for (auto pc: pageCopies)
         CHECK(lp.pushBack(pc));
 
-   CHECK(lp.getPageCopies() == pageCopies);
+    CHECK(lp.getPageCopies() == pageCopies);
+}
+
+TEST(LogPage, sizeTests)
+{
+    LogPage lp(100);
+
+    for (uint32_t i = 0; i < LogPage::MAX_ENTRIES; i++)
+        CHECK(lp.pushBack({ i, i }));
+
+    CHECK(lp.size() == LogPage::MAX_ENTRIES);
+    CHECK(!lp.pushBack({ 1, 1 }));
+}
+
+TEST(LogPage, pushBackIterator)
+{
+    LogPage lp(100);
+
+    for (uint32_t i = 0; i < LogPage::MAX_ENTRIES / 2; i++)
+        CHECK(lp.pushBack({ i, i }));
+
+    auto pageCopies = lp.getPageCopies();
+    CHECK(lp.pushBack(pageCopies.begin(), pageCopies.end()));
+    CHECK(lp.size() == LogPage::MAX_ENTRIES);
+    CHECK(!lp.pushBack({ 1, 1 }));
 }
