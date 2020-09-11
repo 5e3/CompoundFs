4f354809b6b0d66e78f575290ac3b22de2c4417d
5e3 <49342543+5e3@users.noreply.github.com>
2020-09-11 20:45:55 +0200

    make the workflow run...

--- Sandbox/main.cpp
+++ Sandbox/main.cpp
@@ -1,47 +1,50 @@
 
 
 
-template<typename>
-struct Wrapper;
-
-template<typename TRet, typename... TArgs>
-struct Wrapper <TRet(*)(TArgs...)>
-{
-
-    using TSig = TRet(*)(TArgs...);
-
-    Wrapper(TSig func)
-        : m_func(func)
-    {
-    }
-
-    template<typename TRet, typename... TArgs>
-    TRet operator()(TArgs... args)
-    {
-        return m_func(args...);
-    }
-
-    TSig m_func;
-
-};
-
-template <typename TRet, typename... TArgs>
-auto makeWrapper(TRet(func)(TArgs...))
-{
-    return [func](TArgs args...) -> TRet { return func(args...); };
-}
-
-int add(int a, int b)
-{
-    return a + b;
-}
-
+//template<typename>
+//struct Wrapper;
+//
+//template<typename TRet, typename... TArgs>
+//struct Wrapper <TRet(*)(TArgs...)>
+//{
+//
+//    using TSig = TRet(*)(TArgs...);
+//
+//    Wrapper(TSig func)
+//        : m_func(func)
+//    {
+//    }
+//
+//    template<typename TRet, typename... TArgs>
+//    TRet operator()(TArgs... args)
+//    {
+//        return m_func(args...);
+//    }
+//
+//    TSig m_func;
+//
+//};
+//
+//template <typename TRet, typename... TArgs>
+//auto makeWrapper(TRet(func)(TArgs...))
+//{
+//    return [func](TArgs args...) -> TRet { return func(args...); };
+//}
+//
+//int add(int a, int b)
+//{
+//    return a + b;
+//}
+//
+//
+//int main()
+//{
+//    auto f = makeWrapper(add);
+//
+//    f(1, 2);
+//}
 
 int main()
 {
-    auto f = makeWrapper(add);
-
-    f(1, 2);
+    return 0;
 }
-
-
