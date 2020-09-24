b50ff0f7839d21553052292fc95199d0d69992b7
5e3 <49342543+5e3@users.noreply.github.com>
2020-09-24 20:33:54 +0200

    changed to manual triggers for github actions

---
+++ TestDriver/TestPosixFile.cpp
@@ -0,0 +1,10 @@
+
+#include <gtest/gtest.h>
+#include "DiskFileTester.h"
+#include "CompoundFs/PosixFile.h"
+
+#pragma warning(disable : 4996) // disable "'tmpnam': This function or variable may be unsafe."
+
+using namespace TxFs;
+
+INSTANTIATE_TYPED_TEST_SUITE_P(My, DiskFileTester, PosixFile);
