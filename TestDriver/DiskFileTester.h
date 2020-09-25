8ed7a6375400ce24bd766d9afb7ed3380da69cea
5e3 <49342543+5e3@users.noreply.github.com>
2020-09-25 17:43:11 +0200

    fix build...

--- TestDriver/DiskFileTester.h
+++ TestDriver/DiskFileTester.h
@@ -13,7 +13,6 @@ template <typename TDiskFile>
 struct DiskFileTester : ::testing::Test
 {
     std::filesystem::path m_tempFile;
-    using TFile = TDiskFile;
 
     DiskFileTester()
         : m_tempFile(std::filesystem::temp_directory_path() / std::tmpnam(nullptr))
@@ -32,41 +31,41 @@ TYPED_TEST_SUITE_P(DiskFileTester);
 
 TYPED_TEST_P(DiskFileTester, illegalFileNamesThrow)
 {
-    ASSERT_THROW(TFile f("*::", OpenMode::Create), std::system_error);
-    ASSERT_THROW(TFile f("*::", OpenMode::Open), std::system_error);
-    ASSERT_THROW(TFile f("*::", OpenMode::ReadOnly), std::system_error);
+    ASSERT_THROW(TypeParam f("*::", OpenMode::Create), std::system_error);
+    ASSERT_THROW(TypeParam f("*::", OpenMode::Open), std::system_error);
+    ASSERT_THROW(TypeParam f("*::", OpenMode::ReadOnly), std::system_error);
 }
 
 TYPED_TEST_P(DiskFileTester, openNonExistantFilesThrows)
 {
-    ASSERT_THROW(TFile f(m_tempFile, OpenMode::ReadOnly), std::system_error);
+    ASSERT_THROW(TypeParam f(m_tempFile, OpenMode::ReadOnly), std::system_error);
 }
 
 
 
 TYPED_TEST_P(DiskFileTester, createTruncatesFile)
 {
-    TFile file(m_tempFile, OpenMode::Create);
+    TypeParam file(m_tempFile, OpenMode::Create);
     file.newInterval(5);
-    file = TFile(m_tempFile, OpenMode::Create);
+    file = TypeParam(m_tempFile, OpenMode::Create);
     ASSERT_EQ(file.currentSize(), 0);
-    file = TFile();
+    file = TypeParam();
 }
 
 TYPED_TEST_P(DiskFileTester, canOpenSameFileMoreThanOnce)
 {
-    TFile file(m_tempFile, OpenMode::Create);
+    TypeParam file(m_tempFile, OpenMode::Create);
     {
-        TFile file1(m_tempFile, OpenMode::Open);
-        TFile file2(m_tempFile, OpenMode::ReadOnly);
+        TypeParam file1(m_tempFile, OpenMode::Open);
+        TypeParam file2(m_tempFile, OpenMode::ReadOnly);
     }
-    file = TFile();
+    file = TypeParam();
 }
 
 
 TYPED_TEST_P(DiskFileTester, uninitializedFileThrows)
 {
-    TFile file;
+    TypeParam file;
     ASSERT_THROW(file.newInterval(2), std::exception);
     ASSERT_THROW(file.currentSize(), std::exception);
     ASSERT_THROW(file.flushFile(), std::exception);
@@ -78,11 +77,11 @@ TYPED_TEST_P(DiskFileTester, uninitializedFileThrows)
 TYPED_TEST_P(DiskFileTester, readOnlyFileThrowsOnWriteOps)
 {
     {
-        TFile wfile(m_tempFile, OpenMode::Create);
+        TypeParam wfile(m_tempFile, OpenMode::Create);
         wfile.newInterval(5);
     }
 
-    auto file = TFile(m_tempFile, OpenMode::ReadOnly);
+    auto file = TypeParam(m_tempFile, OpenMode::ReadOnly);
     ASSERT_THROW(file.newInterval(2), std::exception);
     ASSERT_THROW(file.truncate(0), std::exception);
     ByteStringView out("0123456789");
@@ -95,7 +94,7 @@ TYPED_TEST_P(DiskFileTester, canReadWriteBigPages)
     std::vector<uint64_t> out((16 * 3 * 1024 * 1024 - 4096) / sizeof(uint64_t));
     std::iota(out.begin(), out.end(), 0);
 
-    TFile file(m_tempFile, OpenMode::Create);
+    TypeParam file(m_tempFile, OpenMode::Create);
     auto iv = file.newInterval(out.size() * sizeof(uint64_t) / 4096);
     file.writePages(iv, (const uint8_t*) out.data());
 
